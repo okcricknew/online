79633c9e03d3b7948798c94bd8e0b4bbb4e0d628
okcricknew <okcrick2@gmail.com>
2026-09-16 13:19:14 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,15 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Laksh 365 Clone',
+  description: 'Next.js SSR with Firebase',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-100 min-h-screen pb-20">{children}</body>
+    </html>
+  );
+}
+
