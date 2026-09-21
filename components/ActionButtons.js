9554e210b3acb7c13471e4ac8c7b52aebb56315a
okcricknew <okcrick2@gmail.com>
2026-09-21 11:34:12 +0530

    Update ActionButtons.js

--- components/ActionButtons.js
+++ components/ActionButtons.js
@@ -1,11 +1,11 @@
 function Item({ icon, title }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="w-16 h-16 rounded-full border-2 border-black bg-white flex items-center justify-center text-3xl">
+      <div className="w-14 h-14 rounded-full border-2 border-black bg-white flex items-center justify-center text-2xl shadow-sm">
         {icon}
       </div>
 
-      <span className="font-bold mt-2 text-[14px]">
+      <span className="font-bold mt-1 text-[13px] text-black">
         {title}
       </span>
     </div>
@@ -14,8 +14,8 @@ function Item({ icon, title }) {
 
 export default function ActionButtons() {
   return (
-    <div className="px-4 -mt-2">
-      <div className="bg-white border-2 border-black rounded-[20px] py-5 px-2 flex justify-around">
+    <div className="px-4 relative z-20 -mt-6">
+      <div className="bg-white border-2 border-black rounded-[24px] py-4 px-2 flex justify-around shadow-md">
         <Item icon="💰" title="DEPOSIT" />
         <Item icon="🏧" title="WITHDRAW" />
         <Item icon="🟢" title="WHATSAPP" />
