1b297de4948c8a77e0013e88f8460b54342c67e4
okcricknew <okcrick2@gmail.com>
2026-09-22 06:42:01 +0530

    Update MainDashboard.js

--- components/MainDashboard.js
+++ components/MainDashboard.js
@@ -1,20 +1,27 @@
 import Header from '@/components/Header';
 import MarketList from '@/components/MarketList';
 import BottomNav from '@/components/BottomNav';
-import MPINLock from '@/components/MPINLock';
+import MPINForm from '@/components/MPINForm';
 
-export default function MainDashboard() {
+export default function MainDashboard({
+  session,
+  error,
+}) {
+  if (!session.mpinVerified) {
+    return <MPINForm error={error} />;
+  }
+
   return (
-    <MPINLock>
-      <div className="max-w-xl mx-auto relative min-h-screen pb-20 bg-gray-55">
-        <Header />
+    <div className="max-w-xl mx-auto relative min-h-screen pb-20 bg-gray-50">
 
-        <main className="p-4 flex flex-col gap-4">
-          <MarketList />
-        </main>
+      <Header session={session} />
 
-        <BottomNav />
-      </div>
-    </MPINLock>
+      <main className="p-4 flex flex-col gap-4">
+        <MarketList />
+      </main>
+
+      <BottomNav />
+
+    </div>
   );
 }
