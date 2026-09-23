9856579095cade2c244b15e02512c2a88cfbd0fe
okcricknew <okcrick2@gmail.com>
2026-09-23 15:01:46 +0530

    Update MarketAutoRefresh.js

--- components/MarketAutoRefresh.js
+++ components/MarketAutoRefresh.js
@@ -3,17 +3,30 @@
 import { useEffect } from 'react';
 
 export default function MarketAutoRefresh({
-  interval = 30000,
+  interval = 10000,
 }) {
   useEffect(() => {
     const refresh = () => {
+      /*
+       * Next.js current page ko dobara server se
+       * render karega.
+       *
+       * Isse:
+       *
+       * 06:00 → OPEN
+       * 10:00 → CLOSE
+       * 11:00 → CLOSED
+       *
+       * automatically update hoga.
+       */
       window.location.reload();
     };
 
-    const timer = window.setInterval(
-      refresh,
-      interval
-    );
+    const timer =
+      window.setInterval(
+        refresh,
+        interval
+      );
 
     return () => {
       window.clearInterval(timer);
