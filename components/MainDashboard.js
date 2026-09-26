6bcea2044b317a562d0c1696073ba67b975e43a5
okcricknew <okcrick2@gmail.com>
2026-09-26 16:36:38 +0530

    Update MainDashboard.js

--- components/MainDashboard.js
+++ components/MainDashboard.js
@@ -11,7 +11,7 @@ export default function MainDashboard({
   mpinUnlocked,
 }) {
   const dashboard = (
-    <div className="max-w-xl mx-auto relative min-h-dvh pb-24 bg-gray-50">
+    <div className="max-w-xl mx-auto relative min-h-dvh pb-16 bg-gray-50">
 
       <Header
         session={session}
