6ad9337ad89f08bb89aae5ce20fc261ce044283f
okcricknew <okcrick2@gmail.com>
2026-09-26 17:00:18 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,8 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-gray-100 min-h-screen pb-0">{children}</body>
+      <body className="bg-gray-100 min-h-dvh">
+        {children}
+      </body>
     </html>
   );
 }
-
