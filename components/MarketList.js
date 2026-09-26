367c3d4d83148b8193b4eb1c3b6b7963e53cfd83
okcricknew <okcrick2@gmail.com>
2026-09-26 16:14:01 +0530

    Update MarketList.js

--- components/MarketList.js
+++ components/MarketList.js
@@ -62,7 +62,7 @@ export default async function MarketList() {
    */
   if (!games.length) {
     return (
-      <section className="px-3 pb-6">
+      <section className="px-3 pb-0">
         <div className="rounded-2xl bg-white p-6 text-center shadow-sm">
           <p className="text-sm font-semibold text-gray-700">
             No games available right now.
